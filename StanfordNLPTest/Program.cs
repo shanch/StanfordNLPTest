95b4d9740518e1e678b20b29066fab2a71ed4fd8
atsushi yamakawa <shanch@sweet.ocn.ne.jp>
2018-11-16 15:08:09 +0900

    Installed StanfordNLP and made a sample program from the following site. https://sergey-tihon.github.io/Stanford.NLP.NET/StanfordNER.html

---
+++ StanfordNLPTest/Program.cs
@@ -0,0 +1,27 @@
+using System;
+using edu.stanford.nlp.ie.crf;
+
+namespace StanfordNLPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Path to the folder with classifiers models
+            var jarRoot = @"..\..\..\stanford-ner-2016-10-31";
+            var classifiersDirecrory = jarRoot + @"\classifiers";
+
+            // Loading 3 class classifier model
+            var classifier = CRFClassifier.getClassifierNoExceptions(
+                classifiersDirecrory + @"\english.all.3class.distsim.crf.ser.gz");
+
+            var s1 = "Good afternoon Rajat Raina, how are you today?";
+            Console.WriteLine("{0}\n", classifier.classifyToString(s1));
+
+            var s2 = "I go to school at Stanford University, which is located in California.";
+            Console.WriteLine("{0}\n", classifier.classifyWithInlineXML(s2));
+
+            Console.WriteLine("{0}\n", classifier.classifyToString(s2, "xml", true));
+        }
+    }
+}
